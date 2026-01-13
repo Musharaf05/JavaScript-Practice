d3283269a9e217e54b2d7f83156cce62636aa630
Musharaf <musharafali.279@gmail.com>
2026-01-13 15:17:02 +0530

    Added console.js file

---
+++ console.js
@@ -0,0 +1,19 @@
+// console is used to print messages in the browser console
+
+// printing number
+console.log(100);
+
+// printing string
+console.log('Hello');
+
+// printing multiple values at once
+console.log(100, 'Hello', true);
+
+// printing error message
+console.error('Alert'); // It shows this message with red color background in browser console
+
+// printing warning
+console.warn('Warning');// It shows this message with yellow color background in browser console
+
+// printing objects values in the form of table
+console.table({name:'Alice',age:20});
