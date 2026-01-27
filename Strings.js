7972d1f14241d892c5c4c41a024964f4c0ca5b4b
Musharaf <musharafali.279@gmail.com>
2026-01-27 16:43:30 +0530

    Added strings file

---
+++ Strings.js
@@ -0,0 +1,15 @@
+let x
+
+const name='John'
+const age=25
+
+x='Hello, my name is '+name+' and i am '+age+' years old'
+
+// Template literals(It prints the string exactly how you specified by enclosing using backtick(`) )
+x=`Hello, my name is ${name} and i am ${age} years old`
+
+// String Properties and Methods
+const s='Hello World'
+x=s.length
+
+console.log(x)
