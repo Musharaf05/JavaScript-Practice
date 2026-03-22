f8e6447acfdb518308eef205e95fb01c47801b43
Musharaf <musharafali.279@gmail.com>
2026-03-22 19:47:56 +0530

    Learned arrays

---
+++ Arrays.js
@@ -0,0 +1,13 @@
+let x
+
+const numbers=[12, 30, 44, 21, 16, 10]
+const fruits=["Apple","Orange","Watermelon"]
+const mixed=[10,"hello",13]
+
+x=numbers.length // 6 (Gives the number of elements in an array)
+
+fruits[1]="Papaya" // changing array value
+
+x=fruits
+
+console.log(x)
---
+++ Date.js
@@ -0,0 +1,9 @@
+// Let's see about this later
+
+let d 
+
+d=new Date()
+
+d=new Date(2026,1,10) 
+
+console.log(d)
